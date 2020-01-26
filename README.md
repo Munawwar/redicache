c86653c88c98d3f8e9064bdf2a869b10b4bed7cc
Munawwar <munawwarfiroz@hotmail.com>
2020-01-27 00:56:41 +0400

    cache library - first code

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
 # redicache
-Two level caching strategy - Redis + Local - for node.js
+Two level caching strategy - Redis + Local (in process memory) - for node.js
 
-("redicache" sounds like "ready cache" doesn't it? See what I did there? 😜)
+Caching works across any number of node.js processes regardless of architecture as long as they can connect to a common redis cluster (library uses redis pubsub and redlock).
+
+(Why "redicache"? It sounds like "ready cache".. doesn't it? 🤷‍♂️)
+
+## Usage
+
+```js
+const redicache = require('redicache');
+
+// create redis client. please read 'redis' npm package's documentation for this.
+const redis = require('redis');
+const redisClient = redis.createClient({
+  // ...
+});
+
+redicache.init(redisClient);
+// redicache is a singleton. you can't initialize again
+
+const fetchHomePage = async () => {
+  // code to fetch CMS home page if it is not in cache already
+  // and then return it.
+};
+
+const cacheKey = 'cms::homepage'; // good practice to namespace it, since redis is global
+const cacheExpiryTimeInSeconds = Infinity; // never expire
+// alternatively you can set expiry to any number of seconds
+const valueOrError = await redicache.getOrInitCache(
+  cacheKey,
+  fetchHomePage,
+  cacheExpiryTimeInSeconds,
+);
+
+// if you cache for unlimited duration of time, you need a way to refresh
+// it by API (without causing home page downtime)
+const newValueOrError = await redicache.attemptCacheRegeneration(
+  cacheKey,
+  fetchHomePage,
+  cacheExpiryTimeInSeconds,
+);
+
+// name of attemptCacheRegeneration() is funny right? reason is, if another process is
+// running getOrInitCache() and has locked cache key for write purpose, then
+// attemptCacheRegeneration will fail (why? because, the process is already refreshing
+// the cache so why do it again?)
+```
---
+++ index.js
@@ -0,0 +1,383 @@
+// 2 level cacher
+// Level 1 - in process memory
+// Level 2 - redis
+
+const Redlock = require('redlock');
+const bluebird = require('bluebird');
+
+// the default expiry time if expiry is not specified.
+const defaultExpiryInSec = 60 * 60; // 1 hour
+
+// lockTime has to be greater than the worst-case blocking duration..
+// like a super long GC pause or CPU intensive tight loop. 10 mins?
+const CACHE_LOCK_TTL = 10 * 60 * 1000; // 10 mins
+const CACHE_LOCK_EXTENSION_TTL = 30 * 1000; // 30 secs
+
+let redisClient;
+let retryForeverLock;
+let tryOnceLock;
+
+const inMemCache = {};
+function fetchFromLocalCache(cacheKey) {
+  return (inMemCache[cacheKey] || {}).value;
+}
+function saveToLocalCache(cacheKey, value, expiryTimeInSec) {
+  if (value === undefined) {
+    console.warn('Cannot save undefined value to local cache for key', cacheKey);
+    return false;
+  }
+  if (expiryTimeInSec === undefined) {
+    console.warn('Cannot save undefined expiry time to local cache for key', cacheKey);
+    return false;
+  }
+  if (expiryTimeInSec < 0) {
+    console.warn('Expiry time cannot be negative for local cache key', cacheKey);
+    return false;
+  }
+  clearTimeout((inMemCache[cacheKey] || {}).timer);
+  inMemCache[cacheKey] = {
+    value,
+    timer: expiryTimeInSec !== Infinity
+      ? setTimeout(() => {
+        delete inMemCache[cacheKey];
+      }, expiryTimeInSec * 1000)
+      : null,
+  };
+  return true;
+}
+
+async function fetchFromRemoteCache(cacheKey) {
+  let val;
+  try {
+    val = redisClient.getAsync(cacheKey);
+  } catch (err) {
+    console.warn('Error while fetching value from remote cache, for key', cacheKey, ':', err.message);
+  }
+  if (typeof val === 'string') {
+    try {
+      return JSON.parse(val);
+    } catch (err) {
+      console.warn('Could not JSON parse the remote cached value for key', cacheKey, ':', err.message);
+    }
+  }
+  return undefined;
+}
+async function fetchTTLFromRemoteCache(cacheKey) {
+  let val;
+  try {
+    val = redisClient.ttlAsync(cacheKey);
+  } catch (err) {
+    console.warn('Error while fetching value from remote cache, for key', cacheKey, ':', err.message);
+  }
+  if (val === -1) {
+    return Infinity;
+  }
+  if (val > 0) {
+    return val;
+  }
+  // if val -2, then key doesn't exists
+  return undefined;
+}
+
+async function saveToRemoteCache(cacheKey, value, expiryTimeInSec) {
+  if (value === undefined) {
+    console.warn('Cannot save undefined value to remote cache for key', cacheKey);
+    return false;
+  }
+  if (expiryTimeInSec === undefined) {
+    console.warn('Cannot save undefined expiry time to remote cache for key', cacheKey);
+    return false;
+  }
+  if (expiryTimeInSec < 0) {
+    console.warn('Expiry time cannot be negative for local cache key', cacheKey);
+    return false;
+  }
+  try {
+    let ttlParams = [];
+    if (expiryTimeInSec !== Infinity) {
+      ttlParams = ['EX', expiryTimeInSec];
+    }
+    return redisClient.setAsync(
+      cacheKey,
+      JSON.stringify(value),
+      ...ttlParams,
+    );
+  } catch (err) {
+    console.warn('Could not save to remote cache for key', cacheKey, ':', err.message);
+    return false;
+  }
+}
+
+async function refreshLocalCacheFromRemoteCache(cacheKey) {
+  // if not found, check in remote cache
+  const [val, ttlInSec] = await Promise.all([
+    fetchFromRemoteCache(cacheKey),
+    fetchTTLFromRemoteCache(cacheKey),
+  ]);
+  if (val !== undefined && ttlInSec) {
+    saveToLocalCache(cacheKey, val, ttlInSec);
+    return val;
+  }
+  return undefined;
+}
+
+function signalOthersProcessesToRefreshLocalCache(cacheKey) {
+  // tell other processes to that their L1 cache could be stale
+  // why could be? because redis pub-sub doesn't guarentee receiver
+  // to receive messages exactly as order of messages sent.
+  // However it doesn't matter if command is respected always
+  redisClient.publish(
+    'cacheChannel',
+    JSON.stringify({
+      command: 'refreshYourLocalCacheFromRemoteCache',
+      cacheKey,
+    }),
+  );
+}
+
+const getLockKey = cacheKey => `cachelock::${cacheKey}`;
+
+function unlock(lock, lockKey) {
+  return lock.unlock()
+    .catch(err => console.warn('Could not release redis cache lock', lockKey, ':', err.message));
+}
+
+// TODO: currently if redis is down, each process will call fetchLatestValue()
+// independently and parellely and could be prohibitively expensive.
+// In some cases, holding the old cache value and serving potentially stale value is better.
+// Therefore in future, give a config option per cache key, to define the behavior
+// i.e. 2 modes of availability.
+async function redisDownCase(cacheKey, fetchLatestValue, expiryTimeInSec) {
+  let latestValue;
+  try {
+    latestValue = await fetchLatestValue();
+  } catch (err) {
+    console.warn('could not compute latest cache value for', cacheKey, ':', err.message);
+  }
+  if (latestValue !== undefined) {
+    saveToLocalCache(cacheKey, latestValue, expiryTimeInSec);
+  }
+  return latestValue;
+}
+
+// export functions
+
+// FIXME: Errors need code values, rather than purely free text.
+
+async function getOrInitCache(
+  cacheKey,
+  fetchLatestValue,
+  expiryTimeInSec = defaultExpiryInSec,
+) {
+  if (!redisClient) {
+    return new Error('cache library not initialized. you need to first call init() method with redisClient as parameter');
+  }
+  // first check in local cache
+  let val = fetchFromLocalCache(cacheKey);
+  if (val !== undefined) {
+    return val;
+  }
+
+  // if not found, check in remote cache
+  val = await refreshLocalCacheFromRemoteCache(cacheKey);
+  if (val !== undefined) {
+    return val;
+  }
+
+  // if not, then get acquire lock preparing for case where
+  // cache value needs to be saved remotely
+  const lockKey = getLockKey(cacheKey);
+  let lock;
+  try {
+    lock = await retryForeverLock.lock(lockKey, CACHE_LOCK_TTL);
+  } catch (err) {
+    console.warn('Could not acquire cache lock for key', `${cacheKey}. Is redis down? :`, err.message);
+    // redis is down.. so fall back to local cache only
+    return redisDownCase(cacheKey, fetchLatestValue, expiryTimeInSec);
+  }
+
+  // run a lock extension timer in case any code within
+  // lock and unlock code block takes too long.
+  // keep track of lastExtendedTime, since JS setInterval may not run
+  // exactly at the set time due to other blocking scripts or GC pause etc.
+  let lastExendedTime = Date.now();
+  const lockExtensionTimer = setInterval(async () => {
+    const now = Date.now();
+    const timeElapsed = now - lastExendedTime;
+    lock = await lock.extend(timeElapsed);
+    lastExendedTime = now;
+  }, CACHE_LOCK_EXTENSION_TTL);
+
+  // it is possible that this one process has been waiting very long to acquire
+  // lock... by which time another process already updated remote cache.
+  // so to avoid refetching latest value, we can first check for cache value again.
+  val = await refreshLocalCacheFromRemoteCache(cacheKey);
+  if (val !== undefined) {
+    clearInterval(lockExtensionTimer);
+    // async unlock. don't await
+    unlock(lock, lockKey);
+    return val;
+  }
+
+  let latestValue;
+  let error = false;
+  try {
+    latestValue = await fetchLatestValue();
+  } catch (err) {
+    error = true;
+    console.warn('could not compute latest cache value for key', cacheKey, ':', err.message);
+  }
+
+  if (latestValue !== undefined) {
+    saveToLocalCache(cacheKey, latestValue, expiryTimeInSec);
+    // asyncly save to remote cache and release lock in normal cases. don't await
+    saveToRemoteCache(cacheKey, latestValue, expiryTimeInSec);
+  } else if (!error) {
+    console.warn('Cannot cache undefined value for key', cacheKey);
+  }
+
+  clearInterval(lockExtensionTimer);
+  // async unlock. don't await
+  unlock(lock, lockKey);
+
+  return latestValue;
+}
+
+async function attemptCacheRegeneration(
+  cacheKey,
+  fetchLatestValue,
+  expiryTimeInSec = defaultExpiryInSec,
+) {
+  if (!redisClient) {
+    return new Error('cache library not initialized. you need to first call init() method with redisClient as parameter');
+  }
+  const lockKey = getLockKey(cacheKey);
+  let lock;
+
+  // for regeneration purpose, don't allow multiple parallel regeneration requests.
+  // reject requests if already regenerating.
+  try {
+    lock = await tryOnceLock.lock(lockKey, CACHE_LOCK_TTL);
+  } catch (err) {
+    // FIXME: need to differentiate redis down vs already locked
+    return new Error(
+      `Could not acquire cache lock for regeneration for lock key ${lockKey}. `
+      + `Maybe another process has acquired lock or redis is down? : ${err.message}`,
+    );
+  }
+
+  // run a lock extension timer in case external
+  // function fetchLatestValue() takes too long.
+  // keep track of lastExtendedTime, since JS setInterval may not run
+  // exactly at the set time due to other blocking scripts or GC pause etc.
+  let lastExendedTime = Date.now();
+  const lockExtensionTimer = setInterval(async () => {
+    const now = Date.now();
+    const timeElapsed = now - lastExendedTime;
+    lock = await lock.extend(timeElapsed);
+    lastExendedTime = now;
+  }, CACHE_LOCK_EXTENSION_TTL);
+
+  let latestValue;
+  let error;
+  try {
+    latestValue = await fetchLatestValue();
+  } catch (err) {
+    error = new Error(`could not compute latest cache value for key ${cacheKey}`);
+  }
+
+  if (!error) {
+    if (latestValue !== undefined) {
+      // when forcing regeneration, wait for save and unlock and then return.
+      const res = await saveToRemoteCache(cacheKey, latestValue, expiryTimeInSec);
+      if (res === false) {
+        error = new Error('Could not save value to remote cache. So not going to save to local cache either');
+      } else {
+        saveToLocalCache(cacheKey, latestValue, expiryTimeInSec);
+        signalOthersProcessesToRefreshLocalCache(cacheKey);
+      }
+    } else {
+      error = new Error(`Cannot cache undefined value for key ${cacheKey}`);
+    }
+  }
+
+  clearInterval(lockExtensionTimer);
+  // wait for unlock
+  await unlock(lock, lockKey);
+
+  if (error) {
+    return error;
+  }
+  return latestValue;
+}
+
+const exportObject = {
+  // why two methods?
+  // one will wait till cache is initialized in the case where
+  // multiple processes try to init cache simultaneously
+  getOrInitCache,
+  // other one will only attempt once before giving up in the case
+  // where multiple processes try to regenerate cache simultaneously
+  attemptCacheRegeneration,
+};
+
+exportObject.init = function init(_redisClient) {
+  if (redisClient) {
+    return new Error('Cannot initialize twice.');
+  }
+  redisClient = _redisClient;
+  bluebird.promisifyAll(Object.getPrototypeOf(redisClient));
+
+  retryForeverLock = new Redlock(
+    // you should have one client for each independent redis node
+    // or cluster
+    [redisClient],
+    {
+      driftFactor: 0.01, // time in ms
+      // the max number of times Redlock will attempt
+      // to lock a resource before erroring
+      retryCount: -1, // retry forever
+      retryDelay: 400, // time in ms
+      retryJitter: 400, // time in ms
+    },
+  );
+
+  tryOnceLock = new Redlock(
+    // you should have one client for each independent redis node
+    // or cluster
+    [redisClient],
+    {
+      driftFactor: 0.01, // time in ms
+      // the max number of times Redlock will attempt
+      // to lock a resource before erroring
+      retryCount: 0, // retry forever
+      retryDelay: 400, // time in ms
+      retryJitter: 400, // time in ms
+    },
+  );
+
+  // listen to commands from other processes like moments when L1 cache is signalled to be stale
+  redisClient.on('message', (channel, rawMessage) => {
+    let message;
+    try {
+      message = JSON.parse(rawMessage);
+    } catch (err) {
+      // do nothing
+    }
+    if (!message) {
+      return;
+    }
+
+    if (
+      channel === 'cacheChannel'
+      && message.command === 'refreshYourLocalCacheFromRemoteCache'
+      && message.cacheKey
+    ) {
+      refreshLocalCacheFromRemoteCache(message.cacheKey);
+    }
+  });
+  redisClient.subscribe('cacheChannel');
+  return exportObject;
+};
+
+module.exports = exportObject;
